5008169de3de9638123ede4ef679e1d92cc16c94
pau111000 <125993328+pau111000@users.noreply.github.com>
2025-07-14 23:21:12 +0200

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,13 @@
+# Write a sequence of Pyhton instructions that allows to declare 3 variables a,b and c 
+# assigning them respectively the values 1, ‘France’, 36.2 
+# and then display the values of these variables in the console.
+
+a = 1
+b = 'Francia'
+c = 36.2
+
+# Print
+print("El valor de a es:", a)
+print("El valor de b es:", b)
+print("El valor de c es:", c)
+https://github.com/pau111000/python-4-projects.git
