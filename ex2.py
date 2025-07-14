29861602fdae7b0c19cda7e1f4c442f75f1f2f0f
pau111000 <125993328+pau111000@users.noreply.github.com>
2025-07-14 23:30:03 +0200

    Add files via upload
    Exercise 2

---
+++ ex2.py
@@ -0,0 +1,7 @@
+#Write a sequence of Python instructions that allows you to declare a variable ch 
+#by initializing it with the value ‘hello’ and then modify that same variable to contain ‘okay’
+#The program should display the contents of the variable in the console after the modification.
+
+ch = 'hello'
+ch = 'okay'
+print(ch)
