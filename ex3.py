3cec0d16f4dcaff888cfef6b1c1f7cef1d99bc8b
pau111000 <125993328+pau111000@users.noreply.github.com>
2025-07-14 23:35:10 +0200

    Add files via upload
    Exercise 3

---
+++ ex3.py
@@ -0,0 +1,11 @@
+# Write a series of Python instructions that allow to declare 2 variables x and y assigning 
+# them respectively the values 3 and 8.5, 
+# then convert the type of these variables to a string.
+# At the end, the program should display the type of these variables after conversion.
+
+x = 3
+y = 8.5
+x = str(x)
+y = str(y)
+print(type(x))
+print(type(y))
