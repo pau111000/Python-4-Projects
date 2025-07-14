ac59a4bb44b8372119dd80554efb97e1b5b3a8a3
pau111000 <125993328+pau111000@users.noreply.github.com>
2025-07-14 23:38:09 +0200

    Add files via upload
    Exercise 4

---
+++ ex4.py
@@ -0,0 +1,5 @@
+# Write a program that asks the user for his weight in kg and stores it in a variable. 
+# The program must show at the end the weight produced by the user.
+
+weight = input("Please enter your weight in kg: ")
+print("Your weight is:", weight, "kg")
